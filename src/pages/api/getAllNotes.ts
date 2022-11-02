e4591a521b4d9b6fc0d63b3f4018f06b1cb42ad3
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-02 21:38:35 +0100

    export response type

--- src/pages/api/getAllNotes.ts
+++ src/pages/api/getAllNotes.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import notes, { Note } from "data/notes";
 
-type Data = {
+export type GetAllNotesResponse = {
   success: boolean;
   data: Note[];
 };
 
-export default function handler(_: NextApiRequest, res: NextApiResponse<Data>) {
+export default function handler(_: NextApiRequest, res: NextApiResponse<GetAllNotesResponse>) {
   return res.status(200).json({ success: true, data: notes });
 }
