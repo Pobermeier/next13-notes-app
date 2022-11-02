7f280001181845d719c951a6453c92569c0fde89
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-02 21:39:06 +0100

    create NoteListSkeleton-component

---
+++ src/app/(components)/NoteListSkeleton.tsx
@@ -0,0 +1,19 @@
+const NoteListSkeleton = () => {
+  return (
+    <div>
+      <ul className="notes-list skeleton-container">
+        <li className="v-stack">
+          <div className="sidebar-note-list-item skeleton" style={{ height: "5em" }} />
+        </li>
+        <li className="v-stack">
+          <div className="sidebar-note-list-item skeleton" style={{ height: "5em" }} />
+        </li>
+        <li className="v-stack">
+          <div className="sidebar-note-list-item skeleton" style={{ height: "5em" }} />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NoteListSkeleton;
