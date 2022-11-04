e055604f4620d15bf67d15ff6c409d2cd230413b
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-04 15:34:54 +0100

    create TextWithMarkdown-component

---
+++ src/app/(components)/TextWithMarkdown.tsx
@@ -0,0 +1,27 @@
+import { marked } from "marked";
+import sanitizeHtml from "sanitize-html";
+
+type TextWithMarkdownProps = {
+  text: string;
+};
+
+const allowedTags = [...sanitizeHtml.defaults.allowedTags, "img", "h1", "h2", "h3"];
+
+const allowedAttributes = {
+  ...sanitizeHtml.defaults.allowedAttributes,
+  img: ["alt", "src"],
+};
+
+const TextWithMarkdown = ({ text }: TextWithMarkdownProps) => (
+  <div
+    className="text-with-markdown"
+    dangerouslySetInnerHTML={{
+      __html: sanitizeHtml(marked(text), {
+        allowedTags,
+        allowedAttributes,
+      }),
+    }}
+  />
+);
+
+export default TextWithMarkdown;
