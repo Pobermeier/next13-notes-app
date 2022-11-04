1c6e924782c2a0d1f19954b04a7f8c4ff8566ec0
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-04 17:36:43 +0100

    add delete-note functionality

--- src/app/(components)/NotePreview.tsx
+++ src/app/(components)/NotePreview.tsx
@@ -1,13 +1,41 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { DeleteNoteResponse } from "pages/api/deleteNote";
 import TextWithMarkdown from "./TextWithMarkdown";
 
 type NotePreviewProps = {
   body: string;
+  id: string;
 };
 
-const NotePreview = ({ body }: NotePreviewProps) => {
+const deleteNote = async (id: string) => {
+  const res = await fetch(`http://localhost:3000/api/deleteNote?id=${id}`);
+
+  const data = (await res.json()) as DeleteNoteResponse;
+
+  if (!data.success) {
+    throw new Error(data.data as string);
+  }
+
+  return data;
+};
+
+const NotePreview = ({ body, id }: NotePreviewProps) => {
+  const router = useRouter();
+
   return (
     <div className="note-preview">
       <TextWithMarkdown text={body} />
+      <br />
+      <button
+        onClick={async () => {
+          await deleteNote(id);
+          router.push("/");
+          router.refresh();
+        }}
+      >
+        Delete Note
+      </button>
     </div>
   );
 };
