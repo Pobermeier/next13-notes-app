4c9c625b013e1bd0c1c799655cde4d21f789ded6
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-05 17:22:17 +0100

    add rocket smiley 🚀

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div className="note-viewer">
       <div className="note--empty-state">
-        <span className="note-text--empty-state">Click on a note on the left to view it!</span>
+        <span className="note-text--empty-state">Click on a note on the left to view it! 🚀</span>
       </div>
     </div>
   );
